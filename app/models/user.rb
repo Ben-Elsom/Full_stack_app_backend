e49dfd5ed671ff690c4f28159bfb110dd8f04968
Ben-Elsom <79074687+Ben-Elsom@users.noreply.github.com>
2021-07-20 16:53:30 +1000

    made signup function, having trouble with sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :phone_number, presence: true 
+
+    def full_name
+        return self.first_name + " " + self.last_name
+    end
 end
