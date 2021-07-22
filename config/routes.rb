4602907a0c4cbc0aae9bb2251b347c7706ef7f5d
Ben-Elsom <79074687+Ben-Elsom@users.noreply.github.com>
2021-07-22 13:06:45 +1000

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "menu_items#index"
   scope '/api' do 
     get '/menu', to: "menu_items#index"
     post '/menu', to: "menu_items#create"
